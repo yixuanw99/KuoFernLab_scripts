e150d2db6d373718a7c083ea1b40e550c60a66e3
yixuanw99 <yixuan.w99@gmail.com>
2024-06-25 03:52:10 +0800

    Add function is_single_copy() and integrate for locus filtering
    In this update, I introduced a new function is_single_copy() to facilitate the filtering of locus names against a predefined list. The function checks whether a given item (locus) exists in the single_copy_list, which includes specific identifiers like "ApPEFP_C", "OG0000764", and "OG0000806". If the locus is found in this list, the script proceeds with processing; otherwise, it skips further execution for that locus.

--- Variantcall_Phasing/phasing_MS.sh
+++ Variantcall_Phasing/phasing_MS.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 # 定義函數
+is_single_copy() {
+    echo "================= 檢查是否是single_copy =================="
+    local single_copy_list=("ApPEFP_C" "OG0000764" "OG0000806" "OG0001134" "OG0001287" "OG0001587" "OG0003109" "OG0003385" "OG0003734" "OG0003748" "OG0003780" "OG0004009" "OG0004248" "OG0004360" "OG0004561" "OG0004566" "OG0004614" "OG0004903" "OG0004943" "OG0004969" "OG0005419" "OG0005470" "OG0005474" "OG0005881" "OG0006029" "OG0006334" "OG0006337" "OG0006377" "OG0006406" "OG0006606" "OG0006650" "OG0006781" "OG0006950" "OG0006968" "OG0007034" "OG0007059" "OG0007255" "OG0007337" "OG0007419" "OG0007472" "OG0007491" "OG0007526" "OG0007580" "OG0007584" "OG0007607" "OG0007648" "OG0007814" "OG0007872" "OG0008201" "OG0008464" "OG0008477" "OG0008526" "OG0008551" "OG0008618" "OG0008661" "OG0008757" "OG0008877" "OG0008889" "OG0008981" "OG0008997" "OG0009191" "OG0009405" "OG0009430" "OG0009437" "OG0009708" "OG0009761" "OG0009864" "OG0009975" "OG0010004" "OG0010124" "OG0010148" "OG0010266" "OG0010273" "OG0010275" "OG0010383" "OG0010439" "OG0010506" "OG0010519" "OG0010559" "OG0010562" "OG0010691" "OG0010995" "OG0011134" "OG0011178" "OG0011263" "OG0011365" "OG0011418" "OG0011530" "OG0011599" "OG0011616" "OG0011638" "OG0011683" "OG0011739" "OG0011740" "OG0011920" "OG0011936" "OG0011960" "OG0011994" "OG0012575" "OG0012619" "OG0012642" "OG0012644" "OG0012674" "OG0012719" "OG0012796" "OG0012887" "OG0012889" "OG0012896" "OG0012930" "OG0013120" "OG0013744" "OG0013794" "OG0013810" "OG0013859" "OG0013866" "OG0013892" "OG0014169" "OG0014204" "OG0014205" "OG0014282" "OG0015475")
+    local item=$1
+    local found=1
+
+    # 遍歷 single_copy_list 檢查 item 是否在 single_copy_list 中
+    for list_item in "${single_copy_list[@]}"; do
+        if [ "$item" == "$list_item" ]; then
+            found=0
+            break
+        fi
+    done
+
+    return $found
+}
+
+
 check_and_create_dir() {
     echo "================= 檢查並創建目錄 =================="
     dir_name="$1"
@@ -254,7 +272,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-working_dir="/home2/yxwu/Phasing_MS"
+working_dir="/home2/yxwu/Phasing"
 
 
 # 讀取配置文件
@@ -287,6 +305,13 @@ while IFS= read -r line || [ -n "$line" ]; do
             # 解析目錄名稱以獲取 locus
             dir_parts=(${dir//\// })
             locus="${dir_parts[-1]}"
+
+            if is_single_copy "$locus"; then
+                echo "$locus is in single copy list, processing..."
+            else
+                echo "$locus is not in single copy list, skipping..."
+                continue
+            fi
             
             # 構建樣本與基因座位的路徑
             input_file="$dir/$sample/sequences/intron/${locus}_supercontig.fasta"
@@ -328,7 +353,7 @@ while IFS= read -r line || [ -n "$line" ]; do
                 whatshap_visualization "$sample" "$locus"
                 
                 # 生成haplotype並建立一致性序列
-                bcftools_concensus "$input_file" "$sample" "$locus" "$ploidy"
+                bcftools_concensus "$working_dir/$sample/$locus/bwa/${sample}_${locus}_supercontig.fasta" "$sample" "$locus" "$ploidy"
             fi
         fi
     done
